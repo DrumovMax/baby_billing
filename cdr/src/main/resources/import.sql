8211098b7d24b99ca7906d5edf42149eb0f329b3
Maxim Drumov <Drumov.M.A@gmail.com>
2024-04-27 21:19:56 +0300

    Add sql initial data script
    Signed-off-by: Maxim Drumov <Drumov.M.A@gmail.com>

---
+++ cdr/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+insert into subscriber (phone_number, is_romashka) values (79500493300, true);
+insert into subscriber (phone_number, is_romashka) values (79432877546, true);
+insert into subscriber (phone_number, is_romashka) values (79110824307, true);
+insert into subscriber (phone_number, is_romashka) values (79729044156, true);
+insert into subscriber (phone_number, is_romashka) values (79476869787, true);
+insert into subscriber (phone_number, is_romashka) values (79475205873, false);
+insert into subscriber (phone_number, is_romashka) values (79915817146, false);
+insert into subscriber (phone_number, is_romashka) values (79964787047, false);
+insert into subscriber (phone_number, is_romashka) values (79421936652, false);
+insert into subscriber (phone_number, is_romashka) values (79709219751, false);
